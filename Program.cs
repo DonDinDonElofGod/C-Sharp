4297905c2d184319ac7d9de442c067ac026a92ec
DonDinDonElofGod <60737676+DonDinDonElofGod@users.noreply.github.com>
2020-02-08 18:43:54 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime;
 
-namespace Task1._1._1._1
+namespace ConsoleApp1
 {
-    class MyClass        
+    class Program
     {
-                
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число: ");
-            int nb = Console.Read();
-            int Base = 2;                       
-            var q = Convert.ToString(nb, Base);            
-            Console.WriteLine($"Результат перевода в двоичную СС: {q}");
+            Console.WriteLine("Введите первую строку:");
+            string i = (Console.ReadLine());
+            Console.WriteLine("Введите вторую строку:");
+            string a = (Console.ReadLine());
+            int z = 0;
+            for (int o = 0; o < a.Length; o++)
+            {
+            if (a[o] == '*')
+                {
+                    z++;
+                }
+            }            
+            bool u = String.Equals(i, a);                       
+            if (u == true || z>0)
+            {                
+                Console.WriteLine($"<<{i}>> VS <<{a}>> - OK");
+            }
+            else
+            {
+                Console.WriteLine($"<<{i}>> VS <<{a}>> - KO");
+            }                             
             
             Console.ReadKey();
 
-
-        }       
+        }
+        
     }
 }
