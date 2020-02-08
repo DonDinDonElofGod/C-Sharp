9d642d75668de439f72db614f5f653cc1a97a89b
DonDinDonElofGod <60737676+DonDinDonElofGod@users.noreply.github.com>
2020-02-08 18:49:12 +0300

    Add files via upload

---
+++ task1/SRC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._1._1._1
+{
+    class MyClass        
+    {
+                
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int nb = Console.Read();
+            int Base = 2;                       
+            var q = Convert.ToString(nb, Base);            
+            Console.WriteLine($"Результат перевода в двоичную СС: {q}");
+            
+            Console.ReadKey();
+
+
+        }       
+    }
+}
